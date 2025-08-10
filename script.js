a72239e7cbd4096b5f32500ee7648e4b9e53be9e
jesmet <jesmetsunny9@gmail.com>
2025-08-10 18:42:44 +0530

    script.js

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
 function addTask(){
-  if(inputBox.value==''){
-    alert("you must write somthing!");
+  if(inputBox.value === ''){
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"; // Unicode for multiplication '×' symbol (used as delete button)
+    li.appendChild(span);
+
+    listContainer.appendChild(li);
   }
-  else{
-       let li = document.creatElement("li);
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML ="\u00d7;
-        li.appendchild(span);
-  }
-  inputBox.value="";
-   saveData();
-    
+  inputBox.value = "";
+  saveData();
 }
 
-listContainet.addEventListenen("click", function(e){
-  if(e.target.tagName =="LI"){
+listContainer.addEventListener("click", function(e){
+  if(e.target.tagName === "LI"){
     e.target.classList.toggle("checked");
     saveData();
-  }
-  else if(e.target.tagName === "SPAN"){
+  } else if(e.target.tagName === "SPAN"){
     e.target.parentElement.remove();
     saveData();
   }
-},false);  
+}, false);
+
 function saveData(){
   localStorage.setItem("data", listContainer.innerHTML);
 }
+
 function showTask(){
-  listContainet.innerHTML = localStorage.getItem("data");
+  listContainer.innerHTML = localStorage.getItem("data") || "";
 }
+
 showTask();
