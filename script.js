f5072ef0d6951c58d7bf59f526db90eeec2aee41
jesmet <jesmetsunny9@gmail.com>
2025-08-10 20:07:44 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
-  if(inputBox.value === ''){
+function addTask() {
+  if (inputBox.value.trim() === '') {
     alert("You must write something!");
   } else {
     let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
+    li.textContent = inputBox.value;
 
     let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
+    span.textContent = "\u00d7";
     li.appendChild(span);
 
     listContainer.appendChild(li);
@@ -18,21 +18,21 @@ function addTask(){
   saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-  if(e.target.tagName === "LI"){
+listContainer.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
     saveData();
-  } else if(e.target.tagName === "SPAN"){
+  } else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
     saveData();
   }
-}, false);
+});
 
-function saveData(){
+function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showTask(){
+function showTask() {
   listContainer.innerHTML = localStorage.getItem("data") || "";
 }
 
